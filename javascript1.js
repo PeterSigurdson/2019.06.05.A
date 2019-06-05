c3c9fa1d258428bed386a4a8ae9927a348fed522
peter sigurdson <peter@petersigurdson.net>
2019-06-05 13:50:51 -0400

    1:50 pm

---
+++ javascript1.js
@@ -0,0 +1,12 @@
+var a = 6;
+function test() {
+    var a = 7;
+    function again() {
+        var a = 8;
+        alert(a);  // A
+    }
+    again();
+    alert(a);  // B
+}
+test();
+alert(a);  // C
